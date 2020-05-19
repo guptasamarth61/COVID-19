8eefb69ef6f0234a05763345f3b52ea9d3c1e8c1
guptasamarth61 <f20170467@pilani.bits-pilani.ac.in>
2020-05-20 02:06:08 +0530

    Added minimize

--- estimation.py
+++ estimation.py
@@ -14,7 +14,7 @@ R = list(df['R'])
 days = list(df['days'])
 ns = len(days) #no of time points
 
-def SEIQCR(t, r, alpha, beta, delta2, p1, p2):
+def SEIQCR(t, r, p):
     #parameters
     A = 0.007896
     q = 0.01
@@ -28,6 +28,7 @@ def SEIQCR(t, r, alpha, beta, delta2, p1, p2):
     delta1 = 0.196
 
     S, E, I, Q, C, R = r
+    alpha, beta, delta2, p1, p2 = p
 
     #equations
     fs = A - (delta*S) - (beta*S*I) - (alpha*C*S) - q*S
@@ -38,10 +39,10 @@ def SEIQCR(t, r, alpha, beta, delta2, p1, p2):
     fr = mu1*I + mu2*Q + mu3*C - delta*R
     return fs, fe, fi, fq, fc, fr
 
-def simulate(alpha, beta, delta2, p1, p2):
+def simulate(p):
     est = np.zeros((6, ns))
     for i in range(ns):
-        sol = solve_ivp(SEIQCR, (0, i), (60461803,2703,94, 127, 2948,1), method = 'LSODA', args = (alpha, beta, delta2, p1, p2), min_step = 0.0000001)
+        sol = solve_ivp(SEIQCR, (0, i), (60461803,2703,94, 127, 2948,1), method = 'LSODA', args = [p], min_step = 0.00000001)
         est[0][i] = sol.y[0][-1]
         est[1][i] = sol.y[1][-1]
         est[2][i] = sol.y[2][-1]
@@ -50,25 +51,27 @@ def simulate(alpha, beta, delta2, p1, p2):
         est[5][i] = sol.y[5][-1]
     return est
 
-def objective(alpha, beta, delta2, p1, p2):
-    est = simulate(alpha, beta, delta2, p1, p2)
+def objective(p):
+    est = simulate(p)
     obj = 0.0
     for i in range(ns):
         obj += (((S[i]-est[0][i])/est[0][i])**2) + (((E[i]-est[1][i])/est[1][i])**2) + (((I[i]-est[2][i])/est[2][i])**2) + (((Q[i]-est[3][i])/est[3][i])**2) + (((C[i]-est[4][i])/est[4][i])**2) + (((R[i]-est[5][i])/est[5][i])**2)
     return obj
 
 #parameter initial guess
-alpha = 0.05
-beta = 0.08
+alpha = 0.0005
+beta = 0.0008
 delta2 = 0.2
-p1 = 0.8
+p1 = 0.2
 p2 = 0.5
+p = [alpha, beta, delta2, p1, p2]
 
 # sol = solve_ivp(SEIQCR, (0, 1), (60461803,2703,94, 127, 2948,1), method = 'LSODA', args = (alpha, beta, delta2, p1, p2), min_step = 0.0000001)
 
 #show objective
-# print('Objective function is ' + str(objective(alpha, beta, delta2, p1, p2)) + '\n');
+# print('Objective function is ' + str(objective(p)) + '\n');
 
 #minimize
 bnds = ((0,1),(0,1),(0,1),(0,1),(0,1))
-solution = minimize(objective, (alpha, beta, delta2, p1, p2), method='SLSQP',bounds=bnds)
+solution = minimize(objective, p,bounds=bnds, method = 'Nelder-Mead', options={'disp': True})
+print(solution.x)
